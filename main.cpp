58f0376352d15235bf805f52d2b2ffa2a5d3f100
rlamug <rlamug@mail.csuchico.edu>
2017-10-18 18:34:04 -0700

    Stars

---
+++ main.cpp
@@ -0,0 +1,64 @@
+// Rodolfo Lamug
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+        int row;
+        int col;
+        cout<<"Enter the  number of rows."<<endl;
+        cin>>row;
+        cout<<"Enter the  number of columns."<<endl;
+        cin>>col;
+
+        if(row <= 0)
+        {
+
+                cout<<""<<endl;
+
+        }
+        else if(col <= 0)
+        {
+
+                cout<<""<<endl;
+
+        }
+        else
+        {
+
+                vector< vector<char> >stars ;
+                stars.resize(row);
+                for(int i = 0; i < stars.size(); i++)
+                {
+                        stars[i].resize(col);
+                }
+
+                for(int i = 0; i < stars.size(); i++)
+                {
+
+                        for(int o = 0; o < stars[i].size(); o++){
+
+                                stars[i][o] = '*';
+                        }
+
+                }
+
+                for(int i = 0; i < stars.size(); i++){
+
+                        for(int o = 0; o < stars[i].size(); o++){
+
+                                cout<<stars[i][o];
+
+                        }
+                        cout<<endl;
+
+                }
+
+        }
+
+
+        return 0;
+}
+
