e6cb311e5ccdb1c5f943aa886f1dc1cee9dfc4cc
rlamug <31227368+rlamug@users.noreply.github.com>
2017-10-20 17:42:48 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,45 +6,34 @@ using namespace std;
 
 int main(){
 
-        int row;
+        int row; 
         int col;
-        cout<<"Enter the  number of rows."<<endl;
+        cout<<"Enter the number of rows and columns of stars:"<<endl;
         cin>>row;
-        cout<<"Enter the  number of columns."<<endl;
         cin>>col;
 
-        if(row <= 0)
-        {
-                
-        }
-        else if(col <= 0)
-        {
-                
-        }
-        else
-        {
+        if(row <= 0){
 
-                vector< vector<char> >stars ;
+                
+
+        }else if(col <= 0){
+
+                
+
+        }else{
+
+                vector< vector<char> > stars;
                 stars.resize(row);
-                for(int i = 0; i < stars.size(); i++)
-                {
+                for(int i = 0; i < stars.size(); i++){
+
                         stars[i].resize(col);
-                }
-
-                for(int i = 0; i < stars.size(); i++)
-                {
-
-                        for(int o = 0; o < stars[i].size(); o++){
-
-                                stars[i][o] = '*';
-                        }
 
                 }
-
                 for(int i = 0; i < stars.size(); i++){
 
                         for(int o = 0; o < stars[i].size(); o++){
 
+                                stars[i][o] = '*';
                                 cout<<stars[i][o];
 
                         }
@@ -52,9 +41,9 @@ int main(){
 
                 }
 
+
         }
 
 
         return 0;
 }
-
